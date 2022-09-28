c267a6b7c35dddf8bcafb0dcaa33ec11c7dde95a
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-09-28 09:29:17 -0500

    shrinky

--- ImageProcessing.java
+++ ImageProcessing.java
@@ -8,45 +8,46 @@ import javax.imageio.ImageIO;
 
 public class ImageProcessing {
     public static void main(String[] args) {
- 
+
         String fileName = "py_logo.png";
         int[][] imageData = imgToTwoD(
                 "./python_logo.png");
-        viewImageData(imageData);
-        //
+        // viewImageData(imageData);
+        // -----------------------------------------------
         // int[][] trimmed = trimBorders(imageData, 60);
         // int[][] negative = negativeColor(imageData);
         // int[][] horizontalStrech = stretchHorizontally(imageData);
-        int[][] verticalStrech = stretchVertically(imageData);
-        //
+        // int[][] verticalStrech = stretchVertically(imageData);
+        // int[][] horizonalShrink = shrinkHorizontally(imageData);
+        int[][] verticalShrink = shrinkVertically(imageData);
+        // -------------------------------------------------------------
         // twoDToImage(trimmed, "trimmed_" + fileName);
         // twoDToImage(negative, "negative_" + fileName);
         // twoDToImage(horizontalStrech, "horizontalStretch_" + fileName);
-        twoDToImage(verticalStrech, "verticalStretch_" + fileName);
-        //
+        // twoDToImage(verticalStrech, "verticalStretch_" + fileName);
+        // twoDToImage(horizonalShrink, "horizonalShrink_" + fileName);
+        twoDToImage(verticalShrink, "verticalShrink_" + fileName);
+        // -----------------------------------------------------
         // int[][] allFilters =
         // stretchHorizontally(shrinkVertically(colorFilter(negativeColor(trimBorders(invertImage(imageData),
         // 50)), 200, 20, 40)));
         // Painting with pixels
     }
 
-    // Image Processing Methods
     // ================================================================================================
+    // ## Image Processing Methods
     public static int[][] trimBorders(int[][] imageTwoD, int pixelCount) {
-        // 
         if (imageTwoD.length > pixelCount * 2 && imageTwoD[0].length > pixelCount * 2) {
-            //
             int[][] trimmedImg = new int[imageTwoD.length - pixelCount * 2][imageTwoD[0].length - pixelCount * 2];
-            //
-            for (int i = 0; i < trimmedImg.length; i++) {
-                //
-                for (int j = 0; j < trimmedImg[i].length; j++) {
-                    trimmedImg[i][j] = imageTwoD[i + pixelCount][j + pixelCount];
-                }
-                //-------
-            }
-            //-----------------------
+            // ----
+            for (int v = 0; v < trimmedImg.length; v++) {
+                for (int h = 0; h < trimmedImg[v].length; h++) {
+                    trimmedImg[v][h] = imageTwoD[v + pixelCount][h + pixelCount];
+                } // -------
+            } // -----------------------
+              // RETURN
             return trimmedImg;
+            // ------------------------
         } else {
             System.out.println("Cannot trim that many pixels from the given image.");
             return imageTwoD;
@@ -58,96 +59,154 @@ public class ImageProcessing {
         // scoped vars
         int[][] negativeImg = new int[imageTwoD.length][imageTwoD[0].length];
         // ----------------
-        for (int i = 0; i < imageTwoD.length; i++) {
-            //
-            for (int u = 1; u < imageTwoD[i].length; u++) {
+        try {
+            for (int v = 0; v < imageTwoD.length; v++) {
                 //
-                int[] rgba = getRGBAFromPixel(imageTwoD[i][u]);
-                rgba[0] = 255 - rgba[0];
-                rgba[1] = 255 - rgba[1];
-                rgba[2] = 255 - rgba[2];
-                negativeImg[i][u] = getColorIntValFromRGBA(rgba);
-            }
-            // -------
+                for (int h = 1; h < imageTwoD[v].length; h++) {
+                    //
+                    int[] rgba = getRGBAFromPixel(imageTwoD[v][h]);
+                    rgba[0] = 255 - rgba[0];
+                    rgba[1] = 255 - rgba[1];
+                    rgba[2] = 255 - rgba[2];
+                    negativeImg[v][h] = getColorIntValFromRGBA(rgba);
+                } // -------
+            } // --------------------
+            return negativeImg;
+            // --!!--
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
         }
-        // --------------------
-    return negativeImg;
+
     }
 
     // ================================================================================================
     public static int[][] stretchHorizontally(int[][] imageTwoD) {
         // doubles each pixel horizonally
-        int[][] horizontalStrech = new int[imageTwoD.length][imageTwoD[0].length*2];
-        //
-        for (int i = 0; i < imageTwoD.length; i++) {
-            //
-            int pointerOne = 0;
-            // 
-            for (int u = 0; u < imageTwoD[0].length; u++) {
-                // doubles each pixel horizontally
-                horizontalStrech[i][pointerOne] = imageTwoD[i][u];
-                horizontalStrech[i][pointerOne+1] = imageTwoD[i][u];
-                pointerOne+=2;
-            }
-
+        int[][] horizontalStrech = new int[imageTwoD.length][imageTwoD[0].length * 2];
+        // ----
+        try {
+            for (int v = 0; v < imageTwoD.length; v++) {
+                int hPointer = 0;
+                // ----
+                for (int h = 0; h < imageTwoD[0].length; h++) {
+                    // doubles each pixel horizontally
+                    horizontalStrech[v][hPointer] = imageTwoD[v][h];
+                    horizontalStrech[v][hPointer + 1] = imageTwoD[v][h];
+                    hPointer += 2;
+                } // -------------------
+            } // ---------------------------------------------
+            return horizontalStrech;
+            // --!!--
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
         }
 
-        return horizontalStrech;
     }
 
+    // ==========================================================================================================
     public static int[][] stretchVertically(int[][] imageTwoD) {
-        // double each pixel vertically
-        int[][] verticalStrech = new int[imageTwoD.length*2][imageTwoD[0].length];
-         //
-        int pointer=0;
-        for (int i = 0; i < imageTwoD.length; i++) {
-            //
-            for (int u = 0; u < imageTwoD[0].length; u++) {
-                //
-                verticalStrech[pointer][u] = imageTwoD[i][u];
-                verticalStrech[pointer+1][u] = imageTwoD[i][u];
-                
-            }
-            pointer+=2;
-        }
-         return verticalStrech;
+        // doubles each pixel vertically
+        int[][] verticalStrech = new int[imageTwoD.length * 2][imageTwoD[0].length];
+        int vPointer = 0;
+        // -------------
+        try {
+            for (int v = 0; v < imageTwoD.length; v++) {
+                for (int h = 0; h < imageTwoD[0].length; h++) {
+                    //
+                    verticalStrech[vPointer][h] = imageTwoD[v][h];
+                    verticalStrech[vPointer + 1][h] = imageTwoD[v][h];
+                } // --------------
+                vPointer += 2;
+            } // ----------------
+            return verticalStrech;
+            // --!!--
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        } // ------
     }
 
+    // =====================================================================================
+    public static int[][] shrinkHorizontally(int[][] imageTwoD) {
+        int ifOdd = imageTwoD[0].length % 2;
+        int[][] horizontalShrink = new int[imageTwoD.length][imageTwoD[0].length / 2 + ifOdd];
+        // -------------
+        try {
+            for (int v = 0; v < horizontalShrink.length; v++) {
+                int hPointer = 0;
+                for (int h = 0; h < horizontalShrink[0].length; h++) {
+                    //
+                    horizontalShrink[v][h] = imageTwoD[v][hPointer];
+                    hPointer += 2;
+                } // --------------
+
+            } // --------------
+            return horizontalShrink;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        } // ------
+    }
+
+    // ===================================================================================
     public static int[][] shrinkVertically(int[][] imageTwoD) {
-        int[][]verticalShrink = new int[imageTwoD.length/2][imageTwoD[0].length];
-
-        return verticalShrink;
+        int ifOdd = imageTwoD.length % 2;
+        int[][] verticalShrink = new int[imageTwoD.length / 2 + ifOdd][imageTwoD[0].length];
+        // -------------
+        try {
+            for (int v = 0; v < verticalShrink.length; v++) {
+                int vPointer = 0;
+                for (int h = 0; h < verticalShrink[0].length; h++) {
+                    //
+                    verticalShrink[v][h] = imageTwoD[vPointer][h];
+                    vPointer += 2;
+                } // --------------
+            } // -------------------------
+            return verticalShrink;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
     }
 
+    // =================================================================================
     public static int[][] invertImage(int[][] imageTwoD) {
         // TODO: Fill in the code for this method
         return null;
     }
 
+    // ====================================================================================
     public static int[][] colorFilter(int[][] imageTwoD, int redChangeValue, int greenChangeValue,
             int blueChangeValue) {
         // TODO: Fill in the code for this method
         return null;
     }
 
+    // ====================================================================================
     // Painting Methods
     public static int[][] paintRandomImage(int[][] canvas) {
         // TODO: Fill in the code for this method
         return null;
     }
 
+    // ====================================================================================
     public static int[][] paintRectangle(int[][] canvas, int width, int height, int rowPosition, int colPosition,
             int color) {
         // TODO: Fill in the code for this method
         return null;
     }
 
+    // ====================================================================================
     public static int[][] generateRectangles(int[][] canvas, int numRectangles) {
         // TODO: Fill in the code for this method
         return null;
     }
 
-    // Utility Methods
+    // ====================================================================================
+    // ## Utility Methods
+    // ----------------
     public static int[][] imgToTwoD(String inputFileOrLink) {
         try {
             BufferedImage image = null;
@@ -165,11 +224,12 @@ public class ImageProcessing {
             int imgRows = image.getHeight();
             int imgCols = image.getWidth();
             int[][] pixelData = new int[imgRows][imgCols];
+            //
             for (int i = 0; i < imgRows; i++) {
                 for (int j = 0; j < imgCols; j++) {
                     pixelData[i][j] = image.getRGB(j, i);
-                }
-            }
+                } // ----
+            } // ------------
             return pixelData;
         } catch (Exception e) {
             System.out.println("Failed to load image: " + e.getLocalizedMessage());
@@ -177,6 +237,8 @@ public class ImageProcessing {
         }
     }
 
+    // =============================== ImageIO
+    // ======================================================
     public static void twoDToImage(int[][] imgData, String fileName) {
         try {
             int imgRows = imgData.length;
@@ -194,11 +256,13 @@ public class ImageProcessing {
         }
     }
 
+    // ====================================================================================================
     public static int[] getRGBAFromPixel(int pixelColorValue) {
         Color pixelColor = new Color(pixelColorValue);
         return new int[] { pixelColor.getRed(), pixelColor.getGreen(), pixelColor.getBlue(), pixelColor.getAlpha() };
     }
 
+    // ===================================================================================================
     public static int getColorIntValFromRGBA(int[] colorData) {
         if (colorData.length == 4) {
             Color color = new Color(colorData[0], colorData[1], colorData[2], colorData[3]);
@@ -209,6 +273,7 @@ public class ImageProcessing {
         }
     }
 
+    // ==================================================================================================
     public static void viewImageData(int[][] imageTwoD) {
         if (imageTwoD.length > 3 && imageTwoD[0].length > 3) {
             int[][] rawPixels = new int[3][3];
